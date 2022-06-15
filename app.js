41893774dd24fb0fbbad9e1dc60b1c5f5b526e97
Mara Braslava <m.braslava@gmail.com>
2022-06-15 12:27:24 +0300

    creating app, config and logger files

---
+++ app.js
@@ -0,0 +1,10 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
+
--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require("./app");
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const server = http.createServer(app)
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
 
-app.use(cors())
-app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.get("/api/blogs", (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.post("/api/blogs", (request, response) => {
+    const blog = new Blog(request.body);
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+const PORT = config.PORT;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
