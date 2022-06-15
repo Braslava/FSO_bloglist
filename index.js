f14aecf91161e57fb64e10099b75fa3ad5fe8436
Mara Braslava <m.braslava@gmail.com>
2022-06-15 12:29:40 +0300

    configure index.js

--- index.js
+++ index.js
@@ -3,41 +3,9 @@ const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-const server = http.createServer(app)
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
+const server = http.createServer(app);
 
 const PORT = config.PORT;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server running on port ${PORT}`);
 });
