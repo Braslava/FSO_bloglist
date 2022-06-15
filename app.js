5c5c45c4c573d18565621c3ac5e6c71586754752
Mara Braslava <m.braslava@gmail.com>
2022-06-15 12:48:24 +0300

    refactoring backend

--- app.js
+++ app.js
@@ -2,41 +2,29 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const blogsRouter = require("./controllers/persons");
-// const middleware = require("./utils/middleware");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
 const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
+mongoose
+    .connect(mongoUrl)
+    .then(() => {
+        console.log("connected to MongoDB");
+    })
+    .catch((error) => {
+        logger.error("error connecting to MongoDB:", error.message);
     });
-});
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.morganConfig);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+
+app.use(middleware.errorHandler);
 
 module.exports = app;
-
