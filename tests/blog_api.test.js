83362d27e24d98923c83d2a30bee83f9affe01ea
Mara Braslava <m.braslava@gmail.com>
2022-06-30 15:17:43 +0300

    test blogs api post requerst

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -50,6 +50,28 @@ test("a specific blog is within the returned blogs", async () => {
     // expect(response.body[0].title).toBe("First blog");
 });
 
+test("a valid blog can be added", async () => {
+    const newBlog = {
+        title: "Added blog",
+        author: "Emily Orange",
+        url: "www.yayblog.com",
+        likes: 14,
+    };
+
+    await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+
+    const titles = response.body.map((r) => r.title);
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1);
+    expect(titles).toContain("Added blog");
+});
+
 afterAll(() => {
     mongoose.connection.close();
 });
